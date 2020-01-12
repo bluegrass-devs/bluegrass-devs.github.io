e8eb4ab6bf9dab6f192a952d191d08290ada54a0
Joe Kratzat <joe.kratzat@gmail.com>
2020-01-11 22:17:16 -0500

    Add company info
    This loads company info from JSON data and populates index.html. It
    isn't fancy, but works for now.

---
+++ js/companyData.js
@@ -0,0 +1,22 @@
+var companyData = [
+  {
+    "name": "Oracle Cloud Infrastructure",
+    "url":"https://www.oracle.com/cloud/",
+    "location": "remote"
+  },
+  {
+    "name": "Digital Ocean",
+    "url":"https://www.digitalocean.com/",
+    "location": "remote"
+  },
+  {
+    "name": "Gitlab",
+    "url":"https://gitlab.com/",
+    "location": "remote"
+  },
+  {
+    "name": "Awesome Inc",
+    "url":"https://www.awesomeinc.org/",
+    "location": "local"
+  }
+]
