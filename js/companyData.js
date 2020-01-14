81f67d84c250df5f8beb93a785d3789fa78ad986
Joe Kratzat <joe.kratzat@gmail.com>
2020-01-13 19:05:58 -0500

    Adding more companies

--- js/companyData.js
+++ js/companyData.js
@@ -29,4 +29,104 @@ var companyData = [
     "url":"http://www.bah.com",
     "location": "remote"
   },
+  {
+    "name": "Rip Road",
+    "url":"http://riproad.com/",
+    "location": "remote"
+  },
+  {
+    "name": "CABEM Technologies",
+    "url":"https://www.cabem.com/",
+    "location": "local"
+  },
+  {
+    "name": "GreekTrack",
+    "url":"https://www.greektrack.com",
+    "location": "local"
+  },
+  {
+    "name": "Parker Hannifin",
+    "url":"https://www.parker.com/portal/site/PARKER/menuitem.223a4a3cce02eb6315731910237ad1ca/?vgnextoid=8e80a35c7575e210VgnVCM10000048021dacRCRD&vgnextfmt=EN",
+    "location": "local"
+  },
+  {
+    "name": "APAX Software",
+    "url": "https://apaxsoftware.com/",
+    "location": "local"
+  },
+  {
+    "name": "Fusioncorp",
+    "url": "https://fusioncorpdesign.com/",
+    "location": "local"
+  },
+  {
+    "name": "HTRI",
+    "url": "https://www.htri.net/",
+    "location": "local"
+  },
+  {
+    "name": "Able Engine",
+    "url": "https://www.ableengine.com/",
+    "location": "local"
+  },
+  {
+    "name": "Prospect Trax",
+    "url": "https://www.prospecttrax.com/",
+    "location": "local"
+  },
+  {
+    "name": "Buds Gun Shop",
+    "url": "https://www.budsgunshop.com/",
+    "location": "local"
+  },
+  {
+    "name": "Nymbl Systems",
+    "url": "http://nymbl.healthcare/",
+    "location": "local"
+  },
+  {
+    "name": "VetData",
+    "url": "https://vetdata.com/",
+    "location": "local"
+  },
+  {
+    "name": "Cycom Data Systems",
+    "url": "https://www.cycominc.com/",
+    "location": "local"
+  },
+  {
+    "name": "Bluegrass Integrated Communications",
+    "url": "http://wearebluegrass.com/",
+    "location": "local"
+  },
+  {
+    "name": "Studio 34 Productions",
+    "url": "https://www.studio34productions.com/",
+    "location": "local"
+  },
+  {
+    "name": "UK HealthCare",
+    "url": "https://ukhealthcare.uky.edu/",
+    "location": "local"
+  },
+  {
+    "name": "Avnet",
+    "url": "https://www.avnet.com/",
+    "location": "local"
+  },
+  {
+    "name": "KET",
+    "url": "https://www.ket.org/",
+    "location": "local"
+  },
+  {
+    "name": "OmniLife",
+    "url": "https://www.omnilife.ai/",
+    "location": "local"
+  },
+  {
+    "name": "Blueframe Technologies",
+    "url": "https://www.blueframetech.com/",
+    "location": "local"
+  },
 ]
